f8897096961902d9a0e7312b9e7946dbd9a5af81
ludens <21203499+ludns@users.noreply.github.com>
2022-02-16 13:18:38 -0800

    update licenses

--- src/L2/PersonaMirror.sol
+++ src/L2/PersonaMirror.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
 import {Base64} from "base64/base64.sol";
--- src/test/mocks/MockL2Bridge.sol
+++ src/test/mocks/MockL2Bridge.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
 
 contract MockL2Bridge {
     address sender;
