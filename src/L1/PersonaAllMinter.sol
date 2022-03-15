3b0c6f2464ab79a12bced51bca469b5bccd1e037
ludens <21203499+ludns@users.noreply.github.com>
2022-03-15 11:43:19 -0600

    added PersonaAllMinter.sol (dev only), and the MockBridge to the abi folder

---
+++ src/L1/PersonaAllMinter.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.8.10;
+
+interface Persona {
+  function mint(address to) external;
+}
+
+contract PersonaAllMinter {
+  Persona public personaContract;
+
+  constructor(
+    address personaContractAddress
+  ) {
+    personaContract = Persona(personaContractAddress);
+  }
+
+  function mintPersona(
+    address minterAddress
+  ) public {
+    personaContract.mint(minterAddress);
+  }
+}
