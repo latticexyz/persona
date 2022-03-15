9002857c3b176e87a3a03742e667022acf3aa2c0
Lint Action <lint-action@samuelmeuli.com>
2022-03-15 17:44:35 +0000

    Fix code style issues with Prettier

--- src/L1/PersonaAllMinter.sol
+++ src/L1/PersonaAllMinter.sol
@@ -1,21 +1,17 @@
 pragma solidity ^0.8.10;
 
 interface Persona {
-  function mint(address to) external;
+    function mint(address to) external;
 }
 
 contract PersonaAllMinter {
-  Persona public personaContract;
+    Persona public personaContract;
 
-  constructor(
-    address personaContractAddress
-  ) {
-    personaContract = Persona(personaContractAddress);
-  }
+    constructor(address personaContractAddress) {
+        personaContract = Persona(personaContractAddress);
+    }
 
-  function mintPersona(
-    address minterAddress
-  ) public {
-    personaContract.mint(minterAddress);
-  }
+    function mintPersona(address minterAddress) public {
+        personaContract.mint(minterAddress);
+    }
 }
